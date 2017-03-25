8d560426b514c576996b990c897de39c1a86f611
znfrench@gmail.com <znfrench@gmail.com>
2017-03-25 11:20:12 -0700

    Got PWM board working with servo.

--- instruments/epsg/code/epsg_single_string/epsg_single_string.ino
+++ instruments/epsg/code/epsg_single_string/epsg_single_string.ino
@@ -4,11 +4,17 @@
 
 #include <stdint.h>
 #include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+#include "Servo.h"
+
 
 // - Defines -------------------------------------------------------------------
+
+#define SERVO 1
+//#define STEPPER 1
 
 #define IIR_DIV 8
 #define TRUNC 3
@@ -19,20 +25,78 @@
 
 // - Local Data ----------------------------------------------------------------
 
+#ifdef STEPPER
 // Create the motor shield object with the default I2C address
 static Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
 static Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 1); // M1 + M2 pins
 //static Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2); // M3 + M4 pins
+#endif
 
+#ifdef SERVO
+Servo servo;
+int pos_deg = 0;
+
+#define SERVO_INPUT A1
+
+
+static Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#define PWM_FREQ 50
+#endif
 
 static int32_t stepper_pos = 0;
 static bool first = true;
 static bool pause = true;
-static int32_t target_filtered = 0;
+static int32_t tarseget_filtered = 0;
+
+
+// 0 = 1.5 ms
+// +90 = 2.0 ms
+// -90 = 1.0 ms
+#define SERVO_P90_MS 2.0
+#define SERVO_M90_MS 1.0
+#define SERVO_0_MS 1.5
 
 
 // - Local Functions -----------------------------------------------------------
+
+
+static void set_servo_pos(int servo, float deg) {
+    // Don't get crazy.
+    if(deg > 90 || deg < -90) {
+        return;
+    }
+#if 0    
+    Serial.print("\ndeg = ");
+    Serial.print(deg);
+#endif
+    // TODO make this all fixed point later
+    float pulse_ms = (float) deg/180 + SERVO_0_MS;
+    float period_ms = 1000*(1.0/PWM_FREQ);
+    float duty_cycle = pulse_ms/period_ms;
+    int pulse_bits = (int) ((2<<11) * (duty_cycle));
+#if 0
+    Serial.print("\nduty cycle = ");
+    Serial.print(duty_cycle);
+
+    Serial.print("\npulse ms = ");
+    Serial.print(pulse_ms);
+    Serial.print("\nperiod ms = ");
+    Serial.print(period_ms);
+
+
+    Serial.print("\nbits = ");
+    Serial.print(pulse_bits);
+    Serial.print("\n");
+#endif
+    Serial.print("servo ");
+    Serial.print(servo);
+    Serial.print(" pulse width ");
+    Serial.print(pulse_bits);
+    Serial.print("\n");
+    pwm.setPWM(servo, 0, pulse_bits);
+}
+
 
 static void control_debug(int pot, int current, int target, int filtered) {
     Serial.print("pot = " );
@@ -52,6 +116,7 @@ static void process_console(void) {
         int in = Serial.read();
 
         switch(in) {
+#ifdef STEPPER
             case 'b':
                 Serial.println("<-");
                 myMotor->step(1, BACKWARD, SINGLE);
@@ -101,6 +166,48 @@ static void process_console(void) {
             case 'r':
                 myMotor->release();
                 break;
+#else
+            case 'b':
+                Serial.println("<-");
+                if(pos_deg > -90) {
+                    pos_deg--;
+                    servo.write(pos_deg + 90);
+  set_servo_pos(0, pos_deg);
+                }
+                Serial.println(pos_deg);
+                Serial.println(analogRead(SERVO_INPUT));
+                break;
+            case 'f':
+                Serial.println("->");
+                if(pos_deg < 90) {
+                    pos_deg++;
+                    servo.write(pos_deg + 90);
+  set_servo_pos(0, pos_deg);
+                }
+                Serial.println(pos_deg);
+                Serial.println(analogRead(SERVO_INPUT));
+                break;
+            case 'B':
+                Serial.println("<-");
+                if(pos_deg > -90) {
+                    pos_deg -= 15;
+                    servo.write(pos_deg + 90);
+  set_servo_pos(0, pos_deg);
+                }
+                Serial.println(pos_deg);
+                Serial.println(analogRead(SERVO_INPUT));
+                break;
+            case 'F':
+                Serial.println("->");
+                if(pos_deg < 90) {
+                    pos_deg += 15;
+                    servo.write(pos_deg + 90);
+  set_servo_pos(0, pos_deg);
+                }
+                Serial.println(pos_deg);
+                Serial.println(analogRead(SERVO_INPUT));
+                break;
+#endif
 
             default:
                 Serial.println("Usage:  b/f:  step (1.8 deg)");
@@ -113,6 +220,7 @@ static void process_console(void) {
 }
 
 
+#ifdef STEPPER
 static void process_control(void) {
     // Read pot.
     uint32_t pot = analogRead(POT_INPUT) >> TRUNC;
@@ -170,8 +278,8 @@ static void process_control(void) {
             stepper_pos += delta;
         }
     }
-
 }
+#endif
 
 
 // - Public Functions ----------------------------------------------------------
@@ -180,14 +288,28 @@ void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("EPSG single string demo");
 
+#ifdef STEPPER
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   myMotor->setSpeed(255);  // 10 rpm   
+#endif
+
+#ifdef SERVO
+  servo.attach(9);
+
+  pwm.begin();
+  pwm.setPWMFreq(PWM_FREQ);
+  set_servo_pos(0, 0);
+#endif
+  Serial.println("Setup finished.");
+  yield();
 }
 
 
 void loop() {
+#ifdef STEPPER
     process_control();
+#endif
     process_console();
 }
